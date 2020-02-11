b1ba405b4618e3ef4fad3cbe14ab7b4e4ff01dec
Jamrod <myfavoritenoisemaker@gmail.com>
2020-02-11 13:26:18 -0700

    built views

--- timeline/views.py
+++ timeline/views.py
@@ -1,7 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Timeline, Time_Capsule
 
 def timeline_list(request):
-    timelines = Timeline.objects.all()
-    return render(request, 'timeline/timeline_list.html' {'timelines': artist})
+    timeline = Timeline.objects.all()
+    return render(request, 'timeline/timeline_list.html', {'timelines': timeline})
+
+def timeline_detail(request, pk):
+    timeline = Timeline.objects.get(id=pk)
+    return render(request, 'timeline/timeline_detail.html', {'timeline': timeline})
+
+def timeline_create(request):
+    if request.method == 'POST':
+        form = TimelineForm(request.POST)
+        if form.is_valid():
+            timeline = form.save()
+            return redirect('timeline_detail', pk=timeline.id)
+    else:
+        form = TimelineForm()
+    return render(request, 'timeline/timeline_form.html', {'form': form})
+
+def timeline_edit(request, pk):
+    timeline = Timeline.objects.get(id-pk)
+    if request.method == 'POST':
+        form = TimelineForm(request.POST, instance=timeline)
+        if form.is_valid():
+            timeline = form.save()
+            return redirect('artist_detail', {'form': form})
+    else:
+        form = TimelineForm(instance=timeline)
+    return render(request, 'timeline/timeline_form.html', {'form': form})
+
+def _delete(request, pk):
+    Timeline.objects.get(id=pk).delete()
+    return redirect('timeline_list')
+
+def capsule_list(request):
+    capsule = Time_Capsule.objects.all()
+    return render(request, 'timeline/capsule_list.html', {'capsules': capsule})
+
+def capsule_detail(request, pk):
+    capsule= Time_Capsule.objects.get(id=pk)
+    return render(request, 'timeline/capsule_detail.html', {'capsule': capsule})
+
+def capsule_create(request):
+    if request.method == 'POST':
+        form = CapsuleForm(request.POST)
+        if form.is_valid():
+            capsule = form.save()
+            return redirect('capsule_detail', pk=capsule.id)
+    else:
+        form = CapsuleForm()
+    return render(request, 'timeline/capsule_form.html', {'form': form})
+
+def capsule_edit(request, pk):
+    capsule = Time_Capsule.objects.get(id-pk)
+    if request.method == 'POST':
+        form = CapsuleForm(request.POST, instance=capsule)
+        if form.is_valid():
+            capsule = form.save()
+            return redirect('capsule_detail', {'form': form})
+    else:
+        form = CapsuleForm(instance=capsule)
+    return render(request, 'timeline/capsule_form.html', {'form': form})
+
+def capsule_delete(request, pk):
+    capsule = Time_Capsule.objects.get(id=pk)
+    timeline = capsule.timeline
+    capsule.delete()
+    return redirect('timeline_detail', pk=timeline.id)
