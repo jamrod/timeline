8d2b6fb4a700417f31eb5f8394ed2ab5d4ff3b37
magbee <david.magbee@me.com>
2020-02-11 14:38:22 -0500

    pre-remote pull commit

--- timeline/admin.py
+++ timeline/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Timeline, Time_Capsule, Timestamp
 # Register your models here.
+admin.site.register(Timeline)
+admin.site.register(Time_Capsule)
+admin.site.register(Timestamp)
